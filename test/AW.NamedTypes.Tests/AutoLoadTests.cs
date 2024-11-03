bca1b0a5fd6942086ba3a66816b03491696ce944
Ajid Welzel <ajid.welzel@gmail.com>
2024-11-03 10:37:30 +0100

    Update AutoLoadTests.cs

--- test/AW.NamedTypes.Tests/AutoLoadTests.cs
+++ test/AW.NamedTypes.Tests/AutoLoadTests.cs
@@ -8,7 +8,7 @@ public class AutoLoadTests
     [TestMethod]
     public void TestAutoLoadTypes()
     {
-        TypeRegistry.Instance.AutoRegisterTypes(Assembly.LoadFrom("LibWithNamedTypes"));
+        TypeRegistry.Instance.AutoRegisterTypes(Assembly.LoadFrom("LibWithNamedTypes.dll"));
         Assert.IsTrue(TypeRegistry.Instance.TryResolveType("my-type", out var type));
 
         var instanceOfMyType = Activator.CreateInstance(type);
